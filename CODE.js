d72b066642b5aa320ff5904d5ac2987a5d00dad7
Melissa Trujillo <mtrujillo93092@uat.edu>
2025-04-13 21:50:26 -0700

    Add files via upload

---
+++ CODE.js
@@ -0,0 +1,24 @@
+/**
+ * Generates a random LifeWave patch protocol and displays it on the page.
+ */
+function generatePatchProtocol() {
+    const patchCombos = [
+      "X39 + Aeon",
+      "X39 + Energy Enhancer",
+      "X49 + X39",
+      "Aeon + Glutathione",
+      "X39 + IceWave",
+      "Alavida + Aeon",
+      "X39 + Carnosine",
+      "X49 + SP6 Complete",
+      "X39 + Silent Nights",
+      "X39 + Nirvana",
+      "Contact Mel to personalize your package 💬"
+    ];
+  
+    const randomIndex = Math.floor(Math.random() * patchCombos.length);
+    const selectedCombo = patchCombos[randomIndex];
+  
+    document.getElementById("patchResult").innerHTML = `<strong>💡 Suggested Protocol:</strong> ${selectedCombo}`;
+  }
+  
